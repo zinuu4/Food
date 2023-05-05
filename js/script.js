fbfcd5e850a3d3ab991db824fce4e5dafa0222ea
zinuu4 <unvm7721@gmail.com>
2023-05-05 16:15:37 +0300

    added modal

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
 
   // Tabs
+
   const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent');
         tabsParent = document.querySelector('.tabheader__items');
@@ -39,65 +40,114 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 
 // Timer
-const deadline = '2023-4-10';
+
+const deadline = '2023-5-10';
 
 function getTimeRemaining(endtime) {
-let days, hours, minutes, seconds;
-const t = Date.parse(endtime) - Date.parse(new Date());
+  let days, hours, minutes, seconds;
+  const t = Date.parse(endtime) - Date.parse(new Date());
 
-if (t <= 0) {
-days = 0;
-hours = 0;
-minutes = 0;
-seconds = 0;
-} else {
-days = Math.floor(t / (1000 * 60 * 60 * 24)),
-hours = Math.floor((t / (1000 * 60 * 60 * 24) % 24)),
-minutes = Math.floor((t / 1000 * 60) % 60),
-seconds = Math.floor((t / 1000) % 60);
-}
+  if (t <= 0) {
+    days = 0;
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+  } else {
+    days = Math.floor(t / (1000 * 60 * 60 * 24)),
+    hours = Math.floor((t / (1000 * 60 * 60 * 24) % 24)),
+    minutes = Math.floor((t / 1000 * 60) % 60),
+    seconds = Math.floor((t / 1000) % 60);
+  }
 
-return {
-'total': t,
-'days': days,
-'hours': hours,
-'minutes': minutes,
-'seconds': seconds
-};
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
 }
 
 function getZero(num) {
-if (num >= 0 && num < 10) {
-return `0${num}`;
-} else {
-return num;
-}
+  if (num >= 0 && num < 10) {
+    return `0${num}`;
+  } else {
+    return num;
+  }
 }
 
 function setClock(selector, endtime) {
-const timer = document.querySelector(selector),
-days = timer.querySelector('#days'),
-hours = timer.querySelector('#hours'),
-minutes = timer.querySelector('#minutes'),
-seconds = timer.querySelector('#seconds'),
-timeInterval = setInterval(updateClock, 1000);
+  const timer = document.querySelector(selector),
+        days = timer.querySelector('#days'),
+        hours = timer.querySelector('#hours'),
+        minutes = timer.querySelector('#minutes'),
+        seconds = timer.querySelector('#seconds'),
+        timeInterval = setInterval(updateClock, 1000);
 
-updateClock();
+  updateClock();
 
-function updateClock() {
-const t = getTimeRemaining(endtime);
+  function updateClock() {
+    const t = getTimeRemaining(endtime);
 
-days.innerHTML = getZero(t.days);
-hours.innerHTML = getZero(t.hours);
-minutes.innerHTML = getZero(t.minutes);
-seconds.innerHTML = getZero(t.seconds);
+    days.innerHTML = getZero(t.days);
+    hours.innerHTML = getZero(t.hours);
+    minutes.innerHTML = getZero(t.minutes);
+    seconds.innerHTML = getZero(t.seconds);
 
-if (t.total <= 0) {
-clearInterval(timeInterval);
-}
-}
+    if (t.total <= 0) {
+      clearInterval(timeInterval);
+    }
+  }
 }
 
 setClock('.timer', deadline);
 
+// Modal
+
+const modalOpenBtn = document.querySelectorAll('[data-modal]'),
+      modal = document.querySelector('.modal'),
+      modalCloseBtn = document.querySelectorAll('[data-close]');
+
+  modalOpenBtn.forEach(item => {
+    item.addEventListener('click', openModal)
+  });
+
+  function openModal() {
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+    document.body.style.overflow = 'hidden';
+    clearInterval(modalTimerId);
+  }
+  function closeModal() {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+    document.body.style.overflow = '';
+  }
+
+  modalCloseBtn.forEach(item => {
+    item.addEventListener('click', closeModal);
+  });
+
+  window.addEventListener("keydown", (e) => {
+    if (e.code == 'Escape' && modal.classList.contains('show')) {
+      closeModal();
+    }
+  });
+
+  modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+
+  const modalTimerId = setTimeout(openModal, 60000);
+
+  function showModalByScroll() {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) { // Прокрученная часть + та часть, которая сейчас на экране >= высоте странички -1 px(так как без этого не во всех браузерах работает)
+      openModal();
+      window.removeEventListener('scroll', showModalByScroll); // После 1 показа модального окна обработчик событий удаляется и окно больше не появится при скроле
+    }
+  }
+
+  window.addEventListener('scroll', showModalByScroll)
 });
