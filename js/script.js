505a0e3ddd2ab2ace4c24fe239f2667780fe3d50
zinuu4 <unvm7721@gmail.com>
2023-05-06 15:10:25 +0300

    some improvements added

--- js/script.js
+++ js/script.js
@@ -116,7 +116,7 @@ const modalOpenBtn = document.querySelectorAll('[data-modal]'),
     modal.classList.add('show');
     modal.classList.remove('hide');
     document.body.style.overflow = 'hidden';
-    clearInterval(modalTimerId);
+    // clearInterval(modalTimerId);
   }
   function closeModal() {
     modal.classList.add('hide');
@@ -154,12 +154,13 @@ const modalOpenBtn = document.querySelectorAll('[data-modal]'),
   // Card classes
 
   class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.src = src;
       this.alt = alt;
       this.title = title;
       this.descr = descr;
       this.price = price;
+      this.classes = classes;
       this.parent = document.querySelector(parentSelector);
       this.transfer = 27;
       this.changeToUAH();
@@ -170,9 +171,15 @@ const modalOpenBtn = document.querySelectorAll('[data-modal]'),
     }
 
     render() {
-      const elemnt = document.createElement('div');
-      elemnt.innerHTML = `
-      <div class="menu__item">
+      const element = document.createElement('div');
+      if (this.classes.length === 0) {
+        this.element = 'menu__item';
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach(className => element.classList.add(className)) // Присваиваем классы в div 'element', который мы создали в строчке выше
+      }
+      
+      element.innerHTML = `
         <img src=${this.src} alt=${this.alt}>
         <h3 class="menu__item-subtitle">${this.title}</h3>
         <div class="menu__item-descr">${this.descr}</div>
@@ -181,9 +188,8 @@ const modalOpenBtn = document.querySelectorAll('[data-modal]'),
           <div class="menu__item-cost">Цена:</div>
           <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
         </div>
-      </div>
       `
-      this.parent.append(elemnt);
+      this.parent.append(element);
     }
   }
 
@@ -193,7 +199,9 @@ const modalOpenBtn = document.querySelectorAll('[data-modal]'),
     'Меню "Фитнес"',
     'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
     9,
-    '.menu .container'
+    '.menu .container',
+    // 'menu__item',
+    // 'big'
   ).render();
 
   new MenuCard(
@@ -202,7 +210,8 @@ const modalOpenBtn = document.querySelectorAll('[data-modal]'),
     'Меню “Премиум”',
     'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
     18,
-    '.menu .container'
+    '.menu .container',
+    'menu__item'
   ).render();
 
   new MenuCard(
@@ -211,6 +220,7 @@ const modalOpenBtn = document.querySelectorAll('[data-modal]'),
     'Меню "Постное"',
     'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
     16,
-    '.menu .container'
+    '.menu .container',
+    'menu__item'
   ).render();
 });
