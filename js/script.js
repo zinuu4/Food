41e7016ec3634fd0256751f2bc069e4179d85755
zinuu4 <unvm7721@gmail.com>
2023-05-12 22:02:02 +0300

    modified slider

--- js/script.js
+++ js/script.js
@@ -286,54 +286,121 @@ const modalOpenBtn = document.querySelectorAll('[data-modal]'),
 
   // Slider
 
-  let slideIndex = 1;
-  const slides = document.querySelectorAll('.offer__slide'),
+  const slider = document.querySelector('.offer__slider'),
+        slides = document.querySelectorAll('.offer__slide'),
         current = document.querySelector('#current'),
         total = document.querySelector('#total'),
         prev = document.querySelector('.offer__slider-prev'),
-        next = document.querySelector('.offer__slider-next');
+        next = document.querySelector('.offer__slider-next'),
+        slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+        slidesField = document.querySelector('.offer__slider-inner'),
+        width = window.getComputedStyle(slidesWrapper).width;
 
-  showSlides(slideIndex);
+  let slideIndex = 1;
+  let offset = 0;
 
-  if (slides.length < 10) {
-    total.textContent = `0${slides.length}`;
-  } else {
-    total.textContent = slides.length;
+  slider.style.position = 'relative';
+
+  const indicators = document.createElement('ol'),
+        dots = [];
+  indicators.classList.add('carousel-indicators');
+  slider.append(indicators);
+
+  for (let i = 0; i < slides.length; i ++) {
+    const dot = document.createElement('li');
+    dot.setAttribute('data-slide-to', i + 1);
+    dot.classList.add('dot');
+
+    if (i == 0) {
+      dot.style.opacity = 1;
+    }
+    indicators.append(dot);
+    dots.push(dot);
   }
 
-  function showSlides(n) {
-    if (n > slides.length) {
-      slideIndex = 1;
-    }
-
-    if (n < 1) {
-      slideIndex = slides.length;
-    }
-
-    slides.forEach(item => {
-      item.classList.add('hide');
-      item.classList.remove('show');
-    })
-
-    slides[slideIndex - 1].classList.add('show');
-    slides[slideIndex - 1].classList.remove('hide');
-
-    if (n < 10) {
+  function dotActive() {
+    dots.forEach(dot => dot.style.opacity = '.5');
+    dots[slideIndex - 1].style.opacity = 1;
+  }
+  function currentIndex() {
+    if (slideIndex < 10) {
       current.textContent = `0${slideIndex}`;
     } else {
       current.textContent = slideIndex;
     }
   }
 
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
+  if (slides.length < 10) {
+    total.textContent = `0${slides.length}`;
+    current.textContent = `0${slideIndex}`;
+  } else {
+    total.textContent = slides.length;
+    current.textContent = slideIndex;
   }
 
-  prev.addEventListener('click', () => {
-    plusSlides(-1);
+  slidesField.style.width = 100 * slides.length + '%';
+  slidesField.style.display = 'flex';
+  slidesField.style.transition = '0.5s all';
+
+  slidesWrapper.style.overflow = 'hidden';
+  
+  slides.forEach(slide => {
+    slide.style.width = width;
   });
 
   next.addEventListener('click', () => {
-    plusSlides(1);
+    if (offset == (+width.slice(0, width.length - 2) * (slides.length - 1))) {
+      offset = 0;
+    } else {
+      offset += +width.slice(0, width.length - 2);
+    }
+
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == slides.length) {
+      slideIndex = 1;
+    } else {
+      slideIndex++;
+    }
+
+    currentIndex()
+
+    dotActive()
+  });
+
+  prev.addEventListener('click', () => {
+    if (offset == 0) {
+      offset += +width.slice(0, width.length - 2) * (slides.length - 1)
+    } else {
+      offset -= +width.slice(0, width.length - 2);
+    }
+
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == 1) {
+      slideIndex = slides.length;
+    } else {
+      slideIndex--;
+    }
+
+    currentIndex()
+
+    dotActive()
+  });
+
+  dots.forEach((dot) => {
+    dot.addEventListener('click', (e) => {
+      const slideTo = e.target.getAttribute('data-slide-to');
+
+      slideIndex = slideTo;
+
+      offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+
+      slidesField.style.transform = `translateX(-${offset}px)`;
+
+      currentIndex()
+
+      dotActive()
+    })
   });
 });
